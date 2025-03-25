42e3606befdfead594ada8acee37273302e9559c
Bananza <31292980+Bananzarama@users.noreply.github.com>
2025-03-25 10:03:37 -0700

    Add files via upload

---
+++ Other/Striping/Pod_striping.py
@@ -0,0 +1,197 @@
+import datetime, json
+from netlab.sync_client import SyncClient
+
+def pod_striping(pod_base=None,parent_pod=None,pod_numbers=32,pod_name_base=None,filename="./pc_clone_spec.json",
+        set1="pc_clone_spec1",set2="pc_clone_spec2",set3="pc_clone_spec3",replacement=True):
+    
+    #argument usage/definitions
+    #pod_base is used to decipher which pod # for the specific set
+    #parent_pod is the pod to be cloned
+    #pod_numbers is the number of pods to clone out
+    #pod numbers will be added in the cloning step from the range utilizing the pod_base
+    #pod_name_base is the string that is re-used by all pods
+    #filename specifies a json file that contains the pc_clone_specs
+    #example pod_name_base="Cyber_Patriots_State_Silver/MiddleSchool_Pod-%02d"
+    #replacement is used to define if it is replacing an existing pod set, if true removes old pods
+
+
+    #begin clone specs easier to edit the clone specs settings below than here
+    #easier to manipulate clone specs
+    with open(filename, 'r') as f:
+        pc_specs=json.load(f)
+
+    pc_clone_specs1 = pc_specs[set1][0]
+    pc_clone_specs2 = pc_specs[set2][0]
+    pc_clone_specs3 = pc_specs[set3][0]
+
+    #end variable to change
+
+    #pod_set is the range of pod destinations to be cloned to
+    pod_set=range(pod_base+1,pod_base+(pod_numbers+1))
+
+    tapi = SyncClient()
+
+    if replacement:
+        #Tack Pods Offline
+        for pod in pod_set:
+            print("taking pod "+str(pod)+" offline")
+            tapi.pod_state_change(pod_id=pod,state='OFFLINE')
+        #Remove Pods
+
+        for pod in pod_set:
+                print("removing pod "+str(pod))
+                tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
+
+    #Creating new pods
+
+
+    for pod in pod_set[::3]:
+        print("Creating pod %02d"%(pod-pod_base))
+        print("cloning to host vh id "+str(pc_clone_specs1['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs1)
+    for pod in pod_set[1::3]:
+        print("Creating pod %02d"%(pod-pod_base))   
+        print("cloning to host vh id "+str(pc_clone_specs2['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs2)
+    for pod in pod_set[2::3]:
+        print("Creating pod %02d"%(pod-pod_base))
+        print("cloning to host vh id "+str(pc_clone_specs3['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs3)
+
+
+
+    #Bringing new pods online
+    for pod in pod_set:
+            print("taking pod "+str(pod)+" online")
+            tapi.pod_state_change(pod_id=pod,state='ONLINE')
+
+
+def new_pod_striping(pod_base=None,parent_pod=None,pod_numbers=32,pod_name_base=None,replacement=False):
+
+    #moved variables from function call, to be globals maybe?
+    filename="./pc_clone_spec.json"
+    set1_1="pc_clone_spec1_1"
+    set1_2="pc_clone_spec1_2"
+    set2_1="pc_clone_spec2_1"
+    set2_2="pc_clone_spec2_2"
+    set3_1="pc_clone_spec3_1"
+    set3_2="pc_clone_spec3_2"
+    set4_1="pc_clone_spec4_1"
+    set4_2="pc_clone_spec4_2"
+    set5_1="pc_clone_spec5_1"
+    set5_2="pc_clone_spec5_2"
+
+    with open(filename, 'r') as f:
+        pc_specs=json.load(f)
+
+    #host1 & host2 have single datastore!
+    pc_clone_specs1_1 = pc_specs[set1_1][0]
+    pc_clone_specs1_2 = pc_specs[set1_1][0]
+    pc_clone_specs2_1 = pc_specs[set2_1][0]
+    pc_clone_specs2_2 = pc_specs[set2_1][0]
+    pc_clone_specs3_1 = pc_specs[set3_1][0]
+    pc_clone_specs3_2 = pc_specs[set3_2][0]
+    pc_clone_specs4_1 = pc_specs[set4_1][0]
+    pc_clone_specs4_2 = pc_specs[set4_2][0]
+    pc_clone_specs5_1 = pc_specs[set5_1][0]
+    pc_clone_specs5_2 = pc_specs[set5_2][0]
+
+    #pod_set is the range of pod destinations to be cloned to
+    pod_set=range(pod_base+1,pod_base+(pod_numbers+1))
+
+    #netlab login
+    tapi = SyncClient()
+
+    if replacement:
+        #Tack Pods Offline
+        for pod in pod_set:
+            print("Taking pod "+str(pod)+" offline")
+            try:
+                tapi.pod_state_change(pod_id=pod,state='OFFLINE')
+            except Exception as e:
+                print("Pod " +str(pod)+" failed to go offline!")
+                print(e)
+
+        #Remove Pods
+        for pod in pod_set:
+                print("removing pod "+str(pod))
+                try:
+                    tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
+                except Exception as e:
+                     print("Pod " +str(pod)+" failed to remove!")
+                     print(e)
+
+
+    #round robin (5 hosts /w datastore alternation)
+    for index, pod in enumerate(pod_set[::5]):
+        if index % 2 == 0:
+            clone_spec = pc_clone_specs1_1
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+        else:
+            clone_spec = pc_clone_specs1_2
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+
+    for index, pod in enumerate(pod_set[1::5]):
+        if index % 2 == 0:
+            clone_spec = pc_clone_specs2_1
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+        else:
+            clone_spec = pc_clone_specs2_2
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+
+    for index, pod in enumerate(pod_set[2::5]):
+        if index % 2 == 0:
+            clone_spec = pc_clone_specs3_1
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+        else:
+            clone_spec = pc_clone_specs3_2
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+
+    for index, pod in enumerate(pod_set[3::5]):
+        if index % 2 == 0:
+            clone_spec = pc_clone_specs4_1
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+        else:
+            clone_spec = pc_clone_specs4_2
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+
+    for index, pod in enumerate(pod_set[4::5]):
+        if index % 2 == 0:
+            clone_spec = pc_clone_specs5_1
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+        else:
+            clone_spec = pc_clone_specs5_2
+            print("Creating pod %02d"%(pod-pod_base)+ " on "+str(clone_spec['clone_datastore']))
+            print("cloning to host vh id "+str(clone_spec['clone_vh_id']))
+            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),pc_clone_specs=clone_spec)
+
+    #Bringing new pods online
+    for pod in pod_set:
+            print("taking pod "+str(pod)+" online")
+            tapi.pod_state_change(pod_id=pod,state='ONLINE')
+
+if __name__ == '__main__':
+     #pod_striping(pod_base=19000,parent_pod=119,pod_name_base="IOT_RaspberryPi_Pod-%02d")
+     new_pod_striping(pod_base=19000,parent_pod=119,pod_name_base="IOT_RaspberryPi_Pod-%02d",replacement=True)
+     print("done")
